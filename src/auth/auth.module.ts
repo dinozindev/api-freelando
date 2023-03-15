93968dbb57e639e51d4782938b48f643dcef4ae5
Vinicios Neves <vinny@macbook.lan>
2023-03-15 18:44:21 +0000

    implementando JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  providers: [AuthService, LocalStrategy],
-  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '600s' },
+    }),
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
